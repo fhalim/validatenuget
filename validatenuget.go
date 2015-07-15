8b4f9b0adb10734b6016632148b9987c0c649427
Fawad Halim <fawad@fawad.net>
2015-07-15 09:51:06 -0500

    Allow specifying prefixes of packages that should be reported on

--- validatenuget.go
+++ validatenuget.go
@@ -6,25 +6,43 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/fhalim/validatenuget/dependencymanagement"
 )
+import linq "github.com/ahmetalpbalkan/go-linq"
 
 func main() {
 	baseDir := flag.String("dir", ".", "Base directory")
+	prefixesStr := flag.String("prefixes", "", "Comma separated prefixes of interesting packages")
 	flag.Parse()
+
+	interestingPrefixes := strings.Split(*prefixesStr, ",")
 
 	state := dependencymanagement.CreatePackagesState()
 
 	processDir(*baseDir, state)
 
 	deps := state.ReconcileDependencies()
-	reportProblems(deps)
+	foundProblems := reportProblems(deps, interestingPrefixes)
+	if foundProblems {
+		os.Exit(2)
+	}
 }
 
-func reportProblems(deps map[string]map[string][]string) {
+func contains(prefixes []string, str string) bool {
+	matchingPrefix := func(prefix linq.T) (bool, error) {
+		return strings.HasPrefix(str, prefix.(string)), nil
+	}
+	result, _ := linq.From(prefixes).AnyWith(matchingPrefix)
+	return result
+}
+
+func reportProblems(deps map[string]map[string][]string, interestingPrefixes []string) bool {
+	foundProblems := false
 	for pkgName, versions := range deps {
-		if len(versions) > 1 {
+		if len(versions) > 1 && contains(interestingPrefixes, pkgName) {
+			foundProblems = true
 			fmt.Printf("Package %v has %v referenced versions\n", pkgName, len(versions))
 			for version, projects := range versions {
 				fmt.Printf("%v:\n", version)
@@ -34,6 +52,7 @@ func reportProblems(deps map[string]map[string][]string) {
 			}
 		}
 	}
+	return foundProblems
 }
 
 func processError(err error) {
